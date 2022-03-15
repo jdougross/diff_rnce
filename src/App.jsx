49a38d89878525550d56bf6cec214d87efe24350
Doug Ross <jdougross@gmail.com>
2022-03-15 13:54:25 -0400

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App (props) {
+  return (
+    <div>
+      <div>Diff_RNCE Audio Player</div>
+      <div>project title</div>
+      <div>audio player section</div>
+      <div>comments section</div>
+    </div>
+  );
+}
+
+export default App;
